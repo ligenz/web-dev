4ecf646f39c3b2ab4b104dda1d95be6426265bbf
ligenz <luke.ligenza@gmail.com>
2017-06-18 15:45:30 +0200

    Update clickbait.php

--- clickbait.php
+++ clickbait.php
@@ -21,10 +21,7 @@
         <title><?php echo TITLE; ?></title>
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -46,16 +43,13 @@
             <?php
             if ( isset( $_POST["fix_submit"] ) ) {
                 
-                // get first value in array returned by checkForClickBait() function
-                // store it in a variable
+               
                 $clickBait = checkForClickBait()[0];
                 
-                // get second value in array returned by checkForClickBait() function
-                // store it in a variable
+                
                 $honestHeadline = checkForClickBait()[1];
                 
-                // call function
-                // pass two arguments in the function
+                
                 displayHonestHeadline( $clickBait, $honestHeadline );
             }
             ?>
